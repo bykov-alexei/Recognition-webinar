c8c0f943f18affee7502335757ffa58d0e0757d1
alexei <bitskoff@gmail.com>
2020-06-19 18:39:13 +0800

    exampl1

---
+++ test/script.py
@@ -0,0 +1,16 @@
+import face_recognition
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+image = face_recognition.load_image_file("guy.jpg")
+face_locations = face_recognition.face_locations(image)
+for location in face_locations:
+    top, right, bottom, left = location
+    x, y = left, top
+    w, h = (right - left), (bottom - top)
+
+fig, ax = plt.subplots(1)
+ax.axis('off')
+rect = patches.Rectangle((x, y), w, h, linewidth=2,edgecolor='r',facecolor='none')
+ax.add_patch(rect)
+ax.imshow(image)
+plt.savefig('detected.jpg', ax)
